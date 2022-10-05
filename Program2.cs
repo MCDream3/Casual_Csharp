299d9f6bdb485de280b75d0eafe3811bb7452069
27tr <114559221+MCDream3@users.noreply.github.com>
2022-10-05 09:35:23 +0200

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Karta_Pracy_Cs_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        //Zad 1
+        int a;
+        a = int.Parse(Console.ReadLine());
+        
+        if (2 > 4)
+        {
+            Console.WriteLine("TAK");
+        }
+        else
+        {
+            Console.WriteLine("NIE");
+        }
+
+            //Zad 2
+         int b;
+         b = int.Parse(Console.ReadLine());
+
+            if (a % 3 == 0)
+            {
+                Console.WriteLine("TAK");
+            }
+            else;
+            {
+                Console.WriteLine("NIE");
+
+                //Zad 3
+                int c;
+                c = int.Parse(Console.ReadLine());
+                
+                if ()
+
+
+
+
+
+            }
+        }
+    }
+}
