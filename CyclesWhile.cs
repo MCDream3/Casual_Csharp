7e20f6b8396d4750e5d1b4ad797bd1a6b500bcb5
27tr <114559221+MCDream3@users.noreply.github.com>
2022-11-23 07:30:54 +0100

    Create CyclesWhile.cs

---
+++ CyclesWhile.cs
@@ -0,0 +1,20 @@
+using System;
+//   public static void Main (string[] args)
+
+// int suma = 0;
+// int a = 1;
+// int b = 1;
+// int x = int.Parse(Console.ReadLine());
+// int n = 0;
+// while(n<x)
+// {
+//   Console.WriteLine(a);
+//   n++;
+//     suma += a;
+//     a += b;
+//     if(n == x) break;
+//     Console.WriteLine(b);
+//     n++;
+//     suma += b;
+//     b += a;
+// }
